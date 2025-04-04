83035465214270a6ebdfce581cc204b7e179c4eb
Zqxg <694917121@qq.com>
2025-04-05 02:52:40 +0800

    [opt] 调整分类视图json结构

--- internal/model/vo/categoryView.go
+++ internal/model/vo/categoryView.go
@@ -1,12 +1,12 @@
 package vo
 
 type CategoryView struct {
-	CId          uint           `json:"cid" gorm:"column:category_id"`            // 分类ID
-	CategoryName string         `json:"categoryName" gorm:"column:category_name"` // 分类名
-	ParentId     uint           `json:"parentId" gorm:"column:parent_id"`         // 父分类ID
-	KbID         uint           `json:"kbId" gorm:"column:kb_id"`                 // 知识库ID
-	Level        int            `json:"level" gorm:"column:level"`                // 层级
-	Children     []CategoryView `json:"children" gorm:"-"`                        // 子分类
+	CId          uint           `json:"cid" gorm:"column:category_id"`             // 分类ID
+	CategoryName string         `json:"category_name" gorm:"column:category_name"` // 分类名
+	ParentId     uint           `json:"parent_id" gorm:"column:parent_id"`         // 父分类ID
+	KbID         uint           `json:"kb_id" gorm:"column:kb_id"`                 // 知识库ID
+	Level        int            `json:"level" gorm:"column:level"`                 // 层级
+	Children     []CategoryView `json:"children" gorm:"-"`                         // 子分类
 }
 
 func (CategoryView) TableName() string {
